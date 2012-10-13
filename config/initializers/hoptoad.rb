07a2da12b551df550ba1ef4e750f8092b1573dd7
Joren <git@jorendegroof.be>
2012-10-13 09:14:48 +0200

    Initial import.

---
+++ config/initializers/hoptoad.rb
@@ -0,0 +1,3 @@
+HoptoadNotifier.configure do |config|
+  config.api_key = '11f329a4c65418dd3c8124b65c291975'
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Capo::Application.config.secret_token = '3fd6965d48d3c5aa50affad94e7254bbbb4d7aecbfffb72650b286034040944d2577dd6969d9c3ae19e7b0ae20d8eb52b180b9cae7840ee682d0dc9609e8adc6'
