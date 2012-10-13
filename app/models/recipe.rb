c70fc473059037386e61f9d2e7a1cf6410a10821
Joren <git@jorendegroof.be>
2012-10-13 16:17:01 +0200

    Adds sync repo to the new recipe folder

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -11,4 +11,14 @@ class Recipe < ActiveRecord::Base
   validates_presence_of :name
   validates_uniqueness_of :name, scope: :version
   validates_format_of :name, with: /^\w+$/
+
+  def sync_repo_to_db(recipes_array)
+   recipes_array.each do |recipe|
+     s = Recipe.find_or_initialize_by_name_and_version(recipe[:name],recipe[:version])
+     s.description = recipe[:description]
+     s.dependencies = recipe[:dependencies]
+     s.code = recipe[:code]
+     s.save!
+   end
+ end
 end
