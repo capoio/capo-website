501aa773dd03a8f2697b319b0a5a620b0c2da4b2
Joren <git@jorendegroof.be>
2012-10-13 17:50:20 +0200

    Adds capfile show

--- app/models/capfile.rb
+++ app/models/capfile.rb
@@ -8,12 +8,19 @@ class Capfile < ActiveRecord::Base
 
   after_create :fetch_content
 
+  def to_param
+    identifier
+  end
+
+  def all_recipes
+    recipes.inject([]) {|all, rec| all << Recipe.where(id: rec.keys.first).where(version: rec.values.first).first}
+  end
+
   private
   def fetch_content
     foo = ''
     recipes.each do |recipe_hash|
       foo << Recipe.where(id: recipe_hash.keys.first).where(version: recipe_hash.values.first).first.code
-      foo << '\n'
     end
     update_attribute :content, foo
   end
