f6f71c8f2bdc475eca148e06d7209133f7b9a851
Steven De Coeyer <steven@banteng.be>
2012-10-13 18:18:36 +0200

    no longer symlinks recipes script

--- lib/clone_repo.rb
+++ lib/clone_repo.rb
@@ -7,11 +7,4 @@ def clone_repo
     retry
   end
   repo.lib.send :command, "pull" # repo.pull is broken in git-1.2.5, see: https://github.com/schacon/ruby-git/issues/32
-
-  # Make sure we have the recipes script
-  recipes_script = "#{Rails.root}/lib/recipes.rb"
-
-  if !File.exists?(recipes_script)
-    FileUtils.symlink("#{Rails.root}/vendor/repo.git/recipes.rb", recipes_script)
-  end
 end
