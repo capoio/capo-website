3ca370e51cc5d6c2a0ea4fdb98cc552e86ff7f83
Steven De Coeyer <steven@banteng.be>
2012-10-13 19:07:17 +0200

    adds lines to copy recipes to db

--- lib/clone_repo.rb
+++ lib/clone_repo.rb
@@ -6,5 +6,11 @@ def clone_repo
     Git.clone("git@github.com:capoio/capo.git", "vendor/repo.git")
     retry
   end
+
+  # Update repo
   repo.lib.send :command, "pull" # repo.pull is broken in git-1.2.5, see: https://github.com/schacon/ruby-git/issues/32
+
+  # Copy to database
+  require "#{Rails.root}/vendor/repo.git/raw_recipes"
+  Recipe.sync_repo_to_db(RawRecipe.load_all)
 end
