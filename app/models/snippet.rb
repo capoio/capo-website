826a2911dc01aa94079d9e3384258563f25cbb53
Steven De Coeyer <steven@banteng.be>
2012-10-13 15:48:29 +0200

    [snippet] serialize :variables and :dependencies should except an Array, not a Hash

--- app/models/snippet.rb
+++ app/models/snippet.rb
@@ -1,8 +1,8 @@
 class Snippet < ActiveRecord::Base
   attr_accessible :code, :dependencies, :description, :name, :variables, :version, :tag_list
 
-  serialize :variables, Hash
-  serialize :dependencies, Hash
+  serialize :variables, Array
+  serialize :dependencies, Array
 
   acts_as_taggable
 end
