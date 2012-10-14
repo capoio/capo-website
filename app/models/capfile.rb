62f7b877ffded493484a903c7785fd191115fd3b
Joren <git@jorendegroof.be>
2012-10-14 22:42:37 +0200

    Shows different parts for settings and code

--- app/models/capfile.rb
+++ app/models/capfile.rb
@@ -6,22 +6,11 @@ class Capfile < ActiveRecord::Base
 
   serialize :recipes, Array
 
-  after_create :fetch_content
-
   def to_param
     identifier
   end
 
   def all_recipes
     recipes.inject([]) {|all, rec| all << Recipe.where(id: rec.keys.first).where(version: rec.values.first).first}
-  end
-
-  private
-  def fetch_content
-    foo = ''
-    recipes.each do |recipe_hash|
-      foo << Recipe.where(id: recipe_hash.keys.first).where(version: recipe_hash.values.first).first.code
-    end
-    update_attribute :content, foo
   end
 end
