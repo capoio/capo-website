23086d96a74ca2843e6f882507046e52b824b91e
Hannes Fostie <hannes@openminds.be>
2012-10-23 10:40:48 +0200

    Changes deploy.rb to use the github repo

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,5 +1,5 @@
 set :application, "capo"
-set :repository,  "git@github.com:railsrumble/r12-team-91.git"
+set :repository,  "git@github.com:capoio/capo-website.git"
 set :branch,      "master"
 
 server 'capo.io', :app, :web, :db, primary: true
