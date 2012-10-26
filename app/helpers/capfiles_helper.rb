0023d5485f1d1094739bfd6748ab4d3f5aceb788
Joren <git@jorendegroof.be>
2012-10-26 10:25:12 +0200

    Fixes classes for recipe buttons

--- app/helpers/capfiles_helper.rb
+++ app/helpers/capfiles_helper.rb
@@ -1,2 +1,11 @@
 module CapfilesHelper
+
+  def recipe_class recipe
+    case
+    when recipe.name == 'default' then 'selected default'
+    when recipe.created_at > 7.days.ago then 'new'
+    else
+      ''
+    end
+  end
 end
